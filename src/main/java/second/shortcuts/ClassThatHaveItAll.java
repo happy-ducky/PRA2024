c4245e2aa7d5df440e5768d6660860a38d3cc29a
s463041 <teremerekuku@gmail.com>
2023-10-16 18:39:50 +0200

    drugi commit

--- src/main/java/second/shortcuts/ClassThatHaveItAll.java
+++ src/main/java/second/shortcuts/ClassThatHaveItAll.java
@@ -1,4 +1,43 @@
 package second.shortcuts;
 
-public class ClassThatHaveItAll implements InterfaceOne {String name;Integer number;@Override
-    public void printMe(String info) {  } }
+import java.util.List;
+
+public class ClassThatHaveItAll implements InterfaceOne {
+    public ClassThatHaveItAll(String name, List<Long> lista, Integer number) {
+        this.name = name;
+        this.lista = lista;
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLista(List<Long> lista) {
+        this.lista = lista;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Long> getLista() {
+        return lista;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    String name;
+    List<Long> lista;
+    Integer number;
+
+    @Override
+    public void printMe(String info) {
+    }
+}
